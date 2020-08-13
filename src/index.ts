d1ed56e461641be05c3796b9fcfc04c24b3f2519
Scott Sullivan <ssulivan11@gmail.com>
2020-08-12 22:02:12 -0500

    better export

--- src/index.ts
+++ src/index.ts
@@ -14,45 +14,39 @@ program
 const regExMatch = program?.match && wcm(program.match, "/");
 const validBranchDirs = program?.dir?.split(" ");
 
-const BranchCheck = () => {
-  getBranch((err: string, branchName: string): (() => void) => {
-    if (err) throw err;
+module.exports = getBranch((err: string, branchName: string): (() => void) => {
+  if (err) throw err;
 
-    let validName = false;
-    // branchName matches array, if provided
-    if (validBranchDirs?.length) {
-      for (let i = 0; i !== validBranchDirs.length; i += 1) {
-        if (branchName.indexOf(validBranchDirs[i]) !== -1) validName = true;
-      }
+  let validName = false;
+  // branchName matches array, if provided
+  if (validBranchDirs?.length) {
+    for (let i = 0; i !== validBranchDirs.length; i += 1) {
+      if (branchName.indexOf(validBranchDirs[i]) !== -1) validName = true;
     }
-    // branchName matches wildcard, if provided
-    if (regExMatch?.test(branchName)) validName = true;
+  }
+  // branchName matches wildcard, if provided
+  if (regExMatch?.test(branchName)) validName = true;
 
-    // error message and exit
-    if (!validName) {
-      const checks = `${validBranchDirs ? validBranchDirs : ""} ${
-        regExMatch ? regExMatch : ""
-      }`;
-      console.log(
-        `${`\n ❌  Git Branch Name Error:`} ${chalk.grey(branchName)} \n`
-      );
-      console.log(
-        `    ▶ Your branch name does not match the validation checks.\n      Please use of the following:`
-      );
-      console.log(`      ${chalk.blue(checks)}\n\n`);
-      return shell.exec(process.exit(1));
-    }
-
-    // success
+  // error message and exit
+  if (!validName) {
+    const checks = `${validBranchDirs ? validBranchDirs : ""} ${
+      regExMatch ? regExMatch : ""
+    }`;
     console.log(
-      `${chalk.green("\n ✔")}  Git Branch Name Valid - ${chalk.grey(
-        branchName
-      )} \n`
+      `${`\n ❌  Git Branch Name Error:`} ${chalk.grey(branchName)} \n`
     );
-    return shell.exec(process.exit(0));
-  });
-};
+    console.log(
+      `    ▶ Your branch name does not match the validation checks.\n      Please use of the following:`
+    );
+    console.log(`      ${chalk.blue(checks)}\n\n`);
+    return shell.exec(process.exit(1));
+  }
 
-BranchCheck();
-
-export default BranchCheck;
+  // success
+  console.log(
+    `${chalk.green("\n ✔")}  Git Branch Name Valid - ${chalk.grey(
+      branchName
+    )} \n`
+  );
+  return shell.exec(process.exit(0));
+});
